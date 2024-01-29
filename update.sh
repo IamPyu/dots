2497f8d9c769b969c8d05e3c82b7c0ac0a3795ec
mixer <mixer@penguin.local>
2024-01-29 15:34:18 -0600

    remove broken fix configuration

--- update.sh
+++ update.sh
@@ -10,5 +10,5 @@ sudo cp -r ~/.emacs.d ./
 cp -r ~/.hammerspoon ./
 
 cp -r ~/.config/alacritty ./config/
-cp -r ~/.config/fish/ ./config/
+cp -r ~/.config/fish ./config/
 cp -r ~/.config/nvim ./config/
